84bc903025a6ac1b2c77e218e1049d11b2cf4632
Guannan-Shen <guannan.shen.ai@gmail.com>
2018-02-13 22:18:55 -0700

    introCS-MIT

---
+++ Python_practice/introcs_MIT_1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 21:41:21 2018
+
+@author: Guannan Shen
+"""
+#%%
+## assign value, math operation and shorcuts in spyder
+pi = 3.14159
+radius = 2.2
+# add comment, following is the circle equation 
+area = pi*(radius**2)
+print(area)
+# control enter to run whole cell
+# "#%%" to add a cell
+
+
+#%%
+#####################
+#### comment lines
+#####################
+##comment many line at a time 
+##like this
+##hlighting all then 
+##using ctrl 1
+#or uncomment using ctrl 1 again
+
+
+##########################
+####  autocomplete names
+#########################
+
+# Spyder can autocomplete names for you
+# start typing a variable name defined in your program and hit tab 
+# before you finish typing -- try it below
+
+a_superrrrrr_long_and_tedious_variable_names = 1
+
+print(a_superrrrrr_long_and_tedious_variable_names * 1001)
+
+#%%
+###########
+## Strings and other objects type to be continued
+############
+
+
+
+
+
