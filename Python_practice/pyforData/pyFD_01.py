28fbb223c83c77bb35110a474f2fa94eb74f8ce3
Guannan-Shen <guannan.shen.ai@gmail.com>
2019-01-19 18:59:59 -0700

    py for data analysis practice 1

---
+++ Python_practice/pyforData/pyFD_01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 18 22:58:33 2019
+
+@author: guanshim
+"""
+
+#%%
+import numpy as np
+data = {i : np.random.randn() for i in range(10)}
+data
