2d15288b456cf7a55c10888e388d146162fdf2cd
Guannan-Shen <guannan.shen.ai@gmail.com>
2019-01-20 05:59:40 -0700

    ipython magic command

---
+++ Python_practice/pyforData/ipython_script_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 19 21:44:43 2019
+
+@author: guanshim
+"""
+
+#%%
+# test script with variables embeded
+def f_multiply(x,y,z):
+    """
+    Multiply 3 numbers
+    
+    Return
+    ------
+    the muliplication
+    """
+    return x*y*z
+x = 2
+y = 1
+z = 9
+
+result = f_multiply(x,y,z)
--- Python_practice/pyforData/pyFD_01.py
+++ Python_practice/pyforData/pyFD_01.py
@@ -5,8 +5,37 @@ Created on Fri Jan 18 22:58:33 2019
 
 @author: guanshim
 """
+#%%
+%pwd
+
+#%%
+import os
+os.getcwd()
+## can not use ~ 
+os.chdir("/home/guanshim/Documents/everyday_code/everday_code/Python_practice/pyforData/")
 
 #%%
 import numpy as np
 data = {i : np.random.randn() for i in range(10)}
 data
+
+## introspection
+print?
+data?
+np.*load*?
+np.load?
+np.load??
+# the docstring is between """ """
+
+
+#%%
+## test of python script
+%run ~/Documents/everyday_code/everday_code/Python_practice/pyforData/ipython_script_test.py
+result
+## Should you wish to give a script access to variables already defined
+# in the interactive IPython namespace, use
+## %run -i instead of plain %run
+
+# ## In the IPython shell, running %matplotlib sets up the integration so you can create
+## multiple plot windows without interfering with the console session:
+%matplotlib
