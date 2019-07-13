32430ff049e8586c2e0699476cdf933879f5f6fb
Guannan-Shen <guannan.shen.ai@gmail.com>
2019-07-13 00:05:04 -0600

    f'' and .format string

--- Python_practice/fluent_py/crash_py.py
+++ Python_practice/fluent_py/crash_py.py
@@ -1,3 +1,31 @@
-book_titile = 'gone with the wind'
+book_titile = 'Gone with the wind'
 # the brace after the .title
+# .title is a method of string
+# Every method is followed by a set of parentheses, because
+# methods often need additional information to do their work.
 print(book_titile.title())
+print(book_titile.upper())
+print(book_titile.lower())
+# check is lower of is upper or not
+# the function method of class title(self)
+# istitle(self)
+print(book_titile.islower())
+print(book_titile.istitle())
+
+# Using Variables in Strings
+first_name = "qiwen"
+last_name = "yu"
+# the space between the first and last name
+# These strings are called f-strings. The f is for format, because Python
+# formats the string by replacing the name of any variable in braces with its
+# value.
+name = f'{first_name} {last_name}'
+print(name.title())
+# If you’re using Python 3.5 or earlier,
+# you’ll need to use the format() method rather than this f syntax.
+print(f'Hello, {name.title()}!')
+# the version before Python 3.5
+print('{} cutie {}'.format(first_name, last_name).title())
+
+# add a tab
+print('\tPython')
--- Python_practice/fluent_py/pythonic_carddeck.py
+++ Python_practice/fluent_py/pythonic_carddeck.py
@@ -7,14 +7,15 @@ class FrenchDeck:
     suits = 'spades diamonds clubs hearts'.split()
     def __init__(self):
         # the _ between self. and cards
-        self._cards = [Card(rank, suit) for suit in self.suits
+        # use self.cards or self._cards
+        self.cards = [Card(rank, suit) for suit in self.suits
                                         for rank in self.ranks]
 
     def __len__(self):
-        return len(self._cards)
+        return len(self.cards)
 
     def __getitem__(self, position):
-        return self._cards[position]
+        return self.cards[position]
 
 beer_card = Card('7', 'diamonds')
 # use the class
