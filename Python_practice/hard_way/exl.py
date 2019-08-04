6d56511296cb96bbecd3750e6425601fed329073
Guannan-Shen <guannan.shen.ai@gmail.com>
2019-08-04 13:37:58 -0600

    pycharm staff

---
+++ Python_practice/hard_way/exl.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+# comment
+print('I "said" do not touch this.')
+print("\n")
+# this will not run
+print("This will run.")
+# math and numbers
+print(4 % 2)
+# the priority and modulo calculator
+print(25 * 3 % 4)
