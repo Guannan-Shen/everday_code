1ad7cd6b745d33ccdfb8ef409bc9b934930493cb
Guannan-Shen <guannan.shen.ai@gmail.com>
2019-08-25 21:59:56 -0600

    sort print then search

--- cpp/binary_search.cpp
+++ cpp/binary_search.cpp
@@ -3,10 +3,19 @@
 using namespace std
 
 void search(int a[], int);
+void sort(int a[], int);
+void print(int a[], int);
 
 int main(){
 	int a[max_len];
+	int Num;
     cout << "Provide less than " << max_len << " integers which are separated by whitespace: " << endl;
-
+    for (int i = 0; i < max_len; i++){
+    	cin >> a[i];
+    }
+    sort(a, max_len);
+    print(a, max_len);
+    cout << "Provide an integer: \n" << endl;
+    cin >> Num;
 	return 0;
 }
